44aa0fb918921621626357ba9e5cfdba36f61261
pedefacilapp <49081069+pedefacilapp@users.noreply.github.com>
2019-03-29 19:49:51 -0300

    Add files via upload

---
+++ js/base.js
@@ -0,0 +1,5 @@
+document.write("<script src='js/sweetalert2.all.min.js'></script>");
+document.write("<script src='js/move-top.js'></script>");
+document.write("<script src='js/easing.js'></script>");
+document.write("<script src='js/bootstrap-3.3.0/bootstrap-3.3.0.min.js'></script>");
+document.write("<script src='js/wow.min.js'></script>");
